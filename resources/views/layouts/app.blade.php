de23488985a254ee1886327b6274d796ef2d7be7
Douglasrs <douglas.rafa.silva@gmail.com>
2021-04-15 16:09:25 -0300

    feat: Created CRUD Pacientes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Sys Clinic - @yield('title')</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -15,17 +14,41 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Font Awesome JS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+            integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous">
+        </script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
+        </script>
+
+        <!-- jQuery 3.1.1 -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Bootstrap 4.5.0 -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
+
+        <!-- DataTables 1.10.21 -->
+        <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
+        <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+        <style>
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: #9C27B0;
+                color: white;
+                text-align: center;
+            }
+
+        </style>
     </head>
     <body class="font-sans antialiased">
+        @section('sidebar')
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
 
             <!-- Page Content -->
             <main>
@@ -33,4 +56,34 @@
             </main>
         </div>
     </body>
+
+    <script>
+    $('.fone').keyup(function() {
+        let input = $(this).val();
+        
+        if (input.length <= 14) {
+            input = input.replace(/\D/g, "")
+            input = input.replace(/(\d{2})(\d)/, "($1) $2")
+            input = input.replace(/(\d{4})(\d)/, "$1-$2")
+        } else {
+            input = input.replace(/\D/g, "")
+            input = input.replace(/(\d{2})(\d)/, "($1) $2")
+            input = input.replace(/(\d{5})(\d)/, "$1-$2")
+        }
+
+        $(this).val(input);
+    });
+
+    $('.cpf').keyup(function() {
+        let input = $(this).val();
+        
+        input = input.replace(/\D/g, "")
+        input = input.replace(/(\d{3})(\d)/, "$1.$2")
+        input = input.replace(/(\d{3})(\d)/, "$1.$2")
+        input = input.replace(/(\d{3})(\d)/, "$1-$2")
+
+        $(this).val(input);
+    });
+    </script>
+    
 </html>
